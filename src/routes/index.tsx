5ef9558a2fa7e064da39425fc8ae8650756cf8c0
David3Emmanuel <david3emmanuel@gmail.com>
2026-04-02 01:51:05 +0100

    Remove starter components

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,5 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { createContext, useContext, useState } from 'react'
-import './index.css'
 
 export const Route = createFileRoute('/')({ component: App })
 
